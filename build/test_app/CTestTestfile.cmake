6985c18f60eb19d33556c3bc52098e851d947fac
omarbassam88 <bassam@pop-os.localdomain>
2020-06-25 03:55:37 +0200

    Cleaning ignored Files

--- build/test_app/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bassam/JOYA/test_app
-# Build directory: /home/bassam/JOYA/build/test_app
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
